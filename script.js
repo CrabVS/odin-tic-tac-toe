d000cad84615c58d7c262a748a7f6a384d3ae73b
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-07-25 22:50:14 +0200

    Add function to check winner

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Controls the flow of the game
 const gameControl = (() => {
     let playerTurn = 0;
+    let winner = null;
+    let gameover = false;
+
+    const getGameStatus = () => {
+        return !gameover;
+    }
 
     const getPlayerSymbol = () => {
         return playerTurn === 0 ? 'O' : 'X';
@@ -10,7 +16,67 @@ const gameControl = (() => {
         playerTurn = playerTurn === 0 ? 1 : 0;
     }
 
-    return { getPlayerSymbol, nextPlayerTurn }
+    const configureWinner = (winningSymbol) => {
+        winner = winningSymbol === 'O' ? 'Player 1' : 'Player 2';
+        gameover = true;
+    }
+
+    const confirmWinningLine = (tileArray) => {
+        if ((tileArray[0] !== '') && (tileArray[0] === tileArray[1]) && (tileArray[0] === tileArray[2])) {
+            configureWinner(tileArray[0]);
+            console.log('winner!');
+        }
+    }
+
+    const checkForWinner = (tiles) => {
+        // Gets and checks for the rows
+        for (let index = 0; index < 3; index++) {
+            let row = [
+                tiles[0 + (index * 3)], 
+                tiles[1 + (index * 3)], 
+                tiles[2 + (index * 3)]
+            ];
+            confirmWinningLine(row);
+        }
+
+        // Gets and checks for the columns
+        for (let index = 0; index < 3; index++) {
+            let col = [
+                tiles[0 + index], 
+                tiles[3 + index], 
+                tiles[6 + index]
+            ];
+            confirmWinningLine(col);
+        }
+
+        // Checks the diagnols
+        if (tiles[4] !== '') {
+            let diag1 = [
+                tiles[0],
+                tiles[4],
+                tiles[8]
+            ];
+
+            let diag2 = [
+                tiles[2],
+                tiles[4],
+                tiles[6]
+            ];
+
+            confirmWinningLine(diag1);
+            confirmWinningLine(diag2);
+        }
+
+        if (!tiles.includes('') && gameControl.getGameStatus()) {
+            console.log('Draw!');
+        }
+
+        if (gameControl.getGameStatus()) {
+            nextPlayerTurn();
+        }
+    }
+
+    return { getPlayerSymbol, checkForWinner, getGameStatus }
 })();
 
 // Controls the board display
@@ -28,12 +94,12 @@ const gameBoard = (() => {
 
     const addTileListener = (tile, tileId) => {
         tile.addEventListener('click', () => {
-            if (tile.textContent === '') {
+            if (tile.textContent === '' && gameControl.getGameStatus()) {
                 const playerSymbol = gameControl.getPlayerSymbol();;
                 tile.textContent = playerSymbol;
                 tile.classList.add(`symbol-${playerSymbol.toLowerCase()}`);
                 tiles[tileId] = playerSymbol;
-                gameControl.nextPlayerTurn();
+                gameControl.checkForWinner(tiles);
             }
         });
     }
