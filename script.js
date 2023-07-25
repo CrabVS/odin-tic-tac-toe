22230ed4f28e593dbfa86f479ef5d6962e6e0b8f
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-07-25 21:58:32 +0200

    Add gameboard and tile generation

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 // Controls the flow of the game
 const gameControl = (() => {
+    let playerTurn = 0;
 
+    const getPlayerTurn = () => {
+        return playerTurn;
+    }
+
+    const nextPlayerTurn = () => {
+        playerTurn = playerTurn === 0 ? 1 : 0;
+    }
+
+    return { getPlayerTurn, nextPlayerTurn }
 })();
 
 const gameBoard = (() => {
+    const gameBoardEl = document.getElementById('gameboard');
+
+    const generateTiles = () => {
+        for (let index = 0; index < 9; index++) {
+            const tile = document.createElement('div');
+            tile.classList.add('tile');
+            gameBoardEl.appendChild(tile);
+        }
+    }
+    
+    const initializeBoard = () => {
+        generateTiles();
+    }
+
+    return { initializeBoard };
 
 })();
 
 const player = () => {
 
 }
+
+gameBoard.initializeBoard();
